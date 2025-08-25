4c98d45ce63d17adcb6cd4a4cb9c9df9cf675e02
Artem Surovtsev <kontershadow@gmail.com>
2025-08-25 22:14:14 +0300

    Add gunicorn to requirements

--- base/settings.py
+++ base/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HOST', '127.0.0.1','PYTHON_HOST')]
+ALLOWED_HOSTS = [os.environ.get('HOST', '127.0.0.1','PYTHON_HOST','surovtsevartem.pythonanywhere.com')]
 
 
 # Application definition
