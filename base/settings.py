87831eadde2fb351064f3eddf01f57952bd967cc
artemsurovtsev <79626248+artemsurowtsew@users.noreply.github.com>
2025-08-26 19:32:27 +0300

    Update settings.py

--- base/settings.py
+++ base/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HOST','surovtsevartem.pythonanywhere.com')]
+ALLOWED_HOSTS = [os.environ.get('HOST','DEPLOY_HOST')]
 
 
 # Application definition
