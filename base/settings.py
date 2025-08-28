a0bdaab12e97603a65c1408a7df77d47e464580a
artemsurovtsev <79626248+artemsurowtsew@users.noreply.github.com>
2025-08-28 08:59:07 +0300

    Update settings.py

--- base/settings.py
+++ base/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "False"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(',')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'courses-production-e394.up.railway.app']
 
 CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://127.0.0.1:8000', 'https://courses-production-e394.up.railway.app']
 # Application definition
